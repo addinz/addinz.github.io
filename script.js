12b0bf8fc79798a74d6be3ab0e9449548a76bfbf
addinz <105861207+addinz@users.noreply.github.com>
2024-04-26 23:49:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const passkey = "josey"; // Replace "your_passkey" with your desired passkey
+
+function checkPasskey() {
+    const passkeyInput = document.getElementById("passkeyInput").value;
+    const errorMessage = document.getElementById("errorMessage");
+
+    if (passkeyInput === passkey) {
+        window.location.href = "success.html"; // Redirect to success page
+    } else {
+        errorMessage.textContent = "Incorrect passkey. Please try again.";
+    }
+}
